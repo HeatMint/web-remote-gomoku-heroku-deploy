d0b819d523fc8ae28f67d000dea4d2625fdb322b
Mike Lu <heatmint2333@gmail.com>
2019-09-17 20:25:07 +0800

    trying

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn app:app --bind 0.0.0.0:$PORT
+web: gunicorn -k flask_sockets.worker app:app
--- app.py
+++ app.py
@@ -146,5 +146,4 @@ def statics(path):
         except IOError:
             pass
 
-port = int(os.environ.get('PORT', 5000))
-socketio.run(app, host='0.0.0.0', port=port)
+socketio.run(app)
