d9a074308f1c61c5617e18466e633287d19e6744
Mingkai Lu <heatmint2333@gmail.com>
2023-10-30 18:30:47 -0400

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,5 +1,5 @@
 window.onload = function () {
-    socket = io(location.protocol + document.domain + ':' + '100/socket');
+    socket = io(location.protocol + document.domain + ':' + '/socket');
     abs_height = window.innerHeight;
     abs_width = window.innerWidth;
 
